dbd35e5518fec08d940ecb894e50a09b4dedc526
mcquo011 <omcquown@gmail.com>
2023-05-09 13:06:49 -0500

    navbar acting up

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -31,69 +31,66 @@ function Navbar() {
   const toggleNavbar = () => {
     setIsCollapsed(!isCollapsed);
   };
-return (
-  <nav className="navbar">
-    <div className="mm-link">M.M.</div>
-    <button onClick={toggleNavbar} className="navbar-toggle">
-      <FontAwesomeIcon icon={faBars} />
-    </button>
-    <ul className={`nav-links ${isCollapsed ? "collapsed" : ""}`}>
-      <li>
-        <Link to="/" className="nav-link" onClick={toggleNavbar}>
-          <FontAwesomeIcon
-            icon={faHome}
-            className="icon"
-            style={{ fontSize: "1.2rem" }}
-          />{" "}
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/about" className="nav-link" onClick={toggleNavbar}>
-          <FontAwesomeIcon
-            icon={faUser}
-            className="icon"
-            style={{ fontSize: "1.2rem" }}
-          />{" "}
-          About
-        </Link>
-      </li>
-      <li>
-        <Link to="/projects" className="nav-link" onClick={toggleNavbar}>
-          <FontAwesomeIcon
-            icon={faFolderOpen}
-            className="icon"
-            style={{ fontSize: "1.2rem" }}
-          />{" "}
-          Projects
-        </Link>
-      </li>
-      <li>
-        <Link to="/resume" className="nav-link" onClick={toggleNavbar}>
-          <FontAwesomeIcon
-            icon={faFileAlt}
-            className="icon"
-            style={{ fontSize: "1.2rem" }}
-          />{" "}
-          Resume
-        </Link>
-      </li>
 
-      <li>
-        <Link to="/contact" className="nav-link" onClick={toggleNavbar}>
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            className="icon"
-            style={{ fontSize: "1.2rem" }}
-          />{" "}
-          Contact
-        </Link>
-      </li>
-    </ul>
-    {/* <Footer /> */}
-  </nav>
-);
-
+  return (
+    <nav className="navbar">
+      <div className="mm-link">M.M.</div>
+      <button onClick={toggleNavbar} className="navbar-toggle">
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+      <ul className={`nav-links ${!isCollapsed ? "" : "collapsed"}`}>
+        <li>
+          <Link to="/" className="nav-link" onClick={toggleNavbar}>
+            <FontAwesomeIcon
+              icon={faHome}
+              className="icon"
+              style={{ fontSize: "1.2rem" }}
+            />{" "}
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="nav-link" onClick={toggleNavbar}>
+            <FontAwesomeIcon
+              icon={faUser}
+              className="icon"
+              style={{ fontSize: "1.2rem" }}
+            />{" "}
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/projects" className="nav-link" onClick={toggleNavbar}>
+            <FontAwesomeIcon
+              icon={faFolderOpen}
+              className="icon"
+              style={{ fontSize: "1.2rem" }}
+            />{" "}
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="/resume" className="nav-link" onClick={toggleNavbar}>
+            <FontAwesomeIcon
+              icon={faFileAlt}
+              className="icon"
+              style={{ fontSize: "1.2rem" }}
+            />{" "}
+            Resume
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="nav-link" onClick={toggleNavbar}>
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className="icon"
+              style={{ fontSize: "1.2rem" }}
+            />{" "}
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* <Footer /> */}
+    </nav>
+  );
 }
-
-export default Navbar;
