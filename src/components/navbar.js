a3734dd655fdbff6bb5b12c999b2bdbf2a498cf2
mcquo011 <omcquown@gmail.com>
2023-05-09 13:37:02 -0500

    more responsive styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,5 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import Footer from "./footer";
-import "../assets/css/navbar.css";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
@@ -10,11 +7,13 @@ import {
   faFolderOpen,
   faEnvelope,
   faFileAlt,
-  faBars
+  faBars,
 } from "@fortawesome/free-solid-svg-icons";
+import "../assets/css/navbar.css";
 
 function Navbar() {
   const [isCollapsed, setIsCollapsed] = useState(window.innerWidth < 768);
+  const location = useLocation();
 
   useEffect(() => {
     const handleResize = () => {
@@ -22,7 +21,6 @@ function Navbar() {
     };
 
     window.addEventListener("resize", handleResize);
-
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -32,68 +30,64 @@ function Navbar() {
     setIsCollapsed(!isCollapsed);
   };
 
+  useEffect(() => {
+    setIsCollapsed(false);
+  }, [location]);
+
+  const navLinks = [
+    {
+      path: "/",
+      label: "Home",
+      icon: faHome,
+    },
+    {
+      path: "/about",
+      label: "About",
+      icon: faUser,
+    },
+    {
+      path: "/projects",
+      label: "Projects",
+      icon: faFolderOpen,
+    },
+    {
+      path: "/resume",
+      label: "Resume",
+      icon: faFileAlt,
+    },
+    {
+      path: "/contact",
+      label: "Contact",
+      icon: faEnvelope,
+    },
+  ];
+
   return (
     <nav className="navbar">
       <div className="mm-link">M.M.</div>
       <button onClick={toggleNavbar} className="navbar-toggle">
         <FontAwesomeIcon icon={faBars} />
       </button>
-      <ul className={`nav-links ${!isCollapsed ? "" : "collapsed"}`}>
-        <li>
-          <Link to="/" className="nav-link" onClick={toggleNavbar}>
-            <FontAwesomeIcon
-              icon={faHome}
-              className="icon"
-              style={{ fontSize: "1.2rem" }}
-            />{" "}
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className="nav-link" onClick={toggleNavbar}>
-            <FontAwesomeIcon
-              icon={faUser}
-              className="icon"
-              style={{ fontSize: "1.2rem" }}
-            />{" "}
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/projects" className="nav-link" onClick={toggleNavbar}>
-            <FontAwesomeIcon
-              icon={faFolderOpen}
-              className="icon"
-              style={{ fontSize: "1.2rem" }}
-            />{" "}
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link to="/resume" className="nav-link" onClick={toggleNavbar}>
-            <FontAwesomeIcon
-              icon={faFileAlt}
-              className="icon"
-              style={{ fontSize: "1.2rem" }}
-            />{" "}
-            Resume
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="nav-link" onClick={toggleNavbar}>
-            <FontAwesomeIcon
-              icon={faEnvelope}
-              className="icon"
-              style={{ fontSize: "1.2rem" }}
-            />{" "}
-            Contact
-          </Link>
-        </li>
+      <ul className={`nav-links ${isCollapsed ? "collapsed" : ""}`}>
+        {navLinks.map((navLink) => (
+          <li key={navLink.path}>
+            <Link
+              to={navLink.path}
+              className="nav-link"
+              onClick={() => setIsCollapsed(false)}
+            >
+              <FontAwesomeIcon
+                icon={navLink.icon}
+                className="icon"
+                style={{ fontSize: "1.2rem" }}
+              />{" "}
+              {navLink.label}
+            </Link>
+          </li>
+        ))}
       </ul>
-      {/* <Footer /> */}
     </nav>
   );
 }
-
 
 export default Navbar;
